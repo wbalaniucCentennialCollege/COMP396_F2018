c324eb236d41d1e4ecfe745bb5304d20233f14ae
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-11-23 19:30:14 -0500

    Initial commit

---
+++ Assets/Grid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public LayerMask notWalkable;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+
+    private Node[,] grid;
+    private float nodeDiameter;
+    private int gridSizeX, gridSizeY;
+
+    void Start()
+    {
+        // Figure out how many nodes can we fit into our grid
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        // Calculate the bottom left corner of our game level
+        Vector3 worldBottomLeft = transform.position - (Vector3.right * gridSizeX / 2) - (Vector3.forward * gridSizeY / 2);
+
+        // Create the actual grid of nodes
+        for(int i = 0; i < gridSizeX; i++)      // Rows
+        {
+            for(int j = 0; j < gridSizeY; j++)  // Columns
+            {
+
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1.0f, gridWorldSize.y));
+    }
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    public bool isWalkable;
+    public Vector3 nodePosition;
+
+    public Node(bool isWalkable, Vector3 nodePosition)
+    {
+        this.isWalkable = isWalkable;
+        this.nodePosition = nodePosition;
+    }
+}
